3f6efdd1910bc35442e679ab0f2d8eebac86dab8
CHUA0528 <choo27283@gmail.com>
2023-12-25 13:07:05 +0800

    LLLLLLLatest

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const client = new MongoClient(uri, {
   });
 
   client.connect();
-  
 // var axios = require('axios');
 // var data = JSON.stringify({
 //     "collection": "users",
@@ -38,7 +37,7 @@ const client = new MongoClient(uri, {
 //     url: 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-poxgq/endpoint/data/v1/action/findOne',
 //     headers: {
 //       'Content-Type': 'application/json',
-//       'Access-Control-Request-Headers': '*',
+//       'Access-Control-Request-Headers': 'port',
 //       'api-key': 'CHUA',
 //     },
 //     data: data
@@ -77,11 +76,16 @@ app.listen(port, () => {
  
  
 //login GET request
-app.get ('/login', async (req, res) => {
+app.post ('/login', async (req, res) => {
 
 
     let data = req.body
     let result = await login(data);
+    //res.setHeader('Content-Type', 'application/pdf');
+    //res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+   // res.status(200).send(pdfData); // Assuming pdfData is a Buffer or binary data
+
+    
 
     const loginUser = result.verify
     const token = result.token
@@ -98,52 +102,274 @@ app.get ('/login', async (req, res) => {
   });
 
 
+// async function login(data) {
+
+//   console.log("Alert! Alert! Someone is logging in!") //Display message to ensure function is called
+//   //Verify username is in the database
+//   let verify = await user.find({user_id : data.user_id}).next();
+//   console.log(verify)
+//   if (verify){
+//     //verify password is correct
+//     const correctPassword = await bcrypt.compare(data.password,verify.password);
+//     if (correctPassword){
+//       token = generateToken(verify)
+//       return{verify,token};
+//     }else{
+//       return ("Wrong password D: Forgotten your password?")
+//     }
+//   }else{
+//     return ("No such user ID found D:")
+// }}
+
 async function login(data) {
 
-  console.log("Alert! Alert! Someone is logging in!") //Display message to ensure function is called
-  //Verify username is in the database
-  let verify = await user.findOne({user_id : data.user_id});
-  console.log(verify)
-  if (verify){
-    //verify password is correct
-    const correctPassword = await bcrypt.compare(data.password,verify.password);
-    if (correctPassword){
-      token = generateToken(verify)
-      return{verify,token};
-    }else{
-      return ("Wrong password D: Forgotten your password?")
+  console.log("Alert! Alert! Someone is logging in!");
+
+  try {
+    // Verify username in the database
+    let verify = await user.findOne({ user_id: data.user_id });
+
+    if (verify) {
+      // Verify password is correct
+      const correctPassword = await bcrypt.compare(data.password, verify.password);
+
+      if (correctPassword) {
+        token = generateToken(verify);
+
+        return { success: true, verify, token };
+      } else {
+        return { success: false, error: "Wrong password D: Forgotten your password?" };
+      }
+    } else {
+      return { success: false, error: "No such user ID found D:" };
     }
-  }else{
-    return ("No such user ID found D:")
-}}
+  } catch (error) {
+    console.error("Error during login:", error);
+    return { success: false, error: "Internal server error during login" };
+  }
+}
+
 
   //generate token for login authentication
   function generateToken(loginProfile){
     return jwt.sign(loginProfile, 'UltimateSuperMegaTitanicBombasticGreatestBestPOGMadSuperiorTheOneandOnlySensationalSecretPassword', { expiresIn: '1h' });
   }
   
-//   //verify generated tokens
-//   function verifyToken(req, res, next){
-//     let header = req.headers.authorization
-//     let token = header.split(' ')[1] //checking header
-//     jwt.verify(token,'UltimateSuperMegaTitanicBombasticGreatestBestPOGMadSuperiorTheOneandOnlySensationalSecretPassword',function(err,decoded){
-//       if(err) {
-//         res.send(errorMessage() + "Token is not valid D:, go to the counter to exchange")
-//         //return
-//       }
-//       req.user = decoded // bar
-  
-//       next()
-//     });
-//   }
-  
-//   //bcrypt functions to generate a hashed password
-//   async function encryption(data){
-//     const salt= await bcrypt.genSalt(saltRounds)
-//     const hashh = await bcrypt.hash(data,salt)
-//     return hashh
-//   }
-  
+//find user GET request
+app.get('/finduser', verifyToken, async (req, res)=>{
+  let authorize = req.user.role //reading the token for authorisation
+  let data = req.body //requesting the data from body
+  //checking the role of user
+  if (authorize == "resident"|| authorize == "security"){
+    res.send(errorMessage() + "\nyou do not have access to finding users!")
+  }else if (authorize == "admin"){
+    const newUser = await findUser(data) //calling the function to find user
+    if (newUser){ //checking if user exist
+      res.send(newUser)
+    }else{
+      res.send(errorMessage() + "User does not exist sadly :[")
+    }
+  //token does not exist
+  }else {
+      res.send(errorMessage() + "Token not valid!")
+    }
+  })
+
+//register user post request
+app.post('/registeruser', verifyToken, async (req, res)=>{
+  let authorize = req.user.role //reading the token for authorisation
+  let data = req.body //requesting the data from body
+  //checking the role of user
+  if (authorize == "security" || authorize == "resident"){
+    res.send("you do not have access to registering users!")
+  }else if (authorize == "admin" ){
+    const newUser = await registerUser(data)
+    if (newUser){ //checking is registration is succesful
+      res.send("Registration request processed, new user is " + newUser.name)
+    }else{
+      res.send(errorMessage() + "User already exists!")
+    }
+  //token does not exist
+  }else {
+      res.send(errorMessage() + "Token not valid!")
+    }
+  })
+
+//update user PATCH request
+app.patch('/updateuser', verifyToken, async (req, res)=>{
+  let authorize = req.user.role //reading the token for authorisation
+  let data = req.body //requesting the data from body
+  //checking the role of user
+  if (authorize == "security" || authorize == "resident"){
+    res.send("you do not have access to update user information!")
+  }else if (authorize == "admin" ){
+    const result = await updateUser(data)
+    if (result){ // checking if the user exist and updated
+      console.log(result)
+      res.send("User updated! " + result.value.name)
+    }else{
+      res.send(errorMessage() + "User does not exist!")
+    }
+  }else {
+      res.send(errorMessage() + "Token is not found!")
+    }
+})
+
+//delete user DELETE request
+app.delete('/deleteuser', verifyToken, async (req, res)=>{
+  let data = req.body
+  let authorize = req.user.role
+  //checking the role of user
+  if (authorize == "security" || authorize == "resident"){
+    res.send("you do not have access to registering users!")
+  }else if (authorize == "admin" ){
+    const lmao = await deleteUser(data)
+    //checking if item is deleted
+    if (lmao.deletedCount == "1"){
+      res.send("user deleted " + data.user_id)
+    }else{
+      res.send(errorMessage() + "Cannot find the user to delete!")
+    }
+  }else {
+      res.send(errorMessage() + "Token not valid!")
+    }
+  }
+)
+
+//register visitor POST request
+app.post('/registervisitor', verifyToken, async (req, res)=>{
+  let authorize = req.user.role
+  let loginUser = req.user.user_id
+  let data = req.body
+  //checking if token is valid
+  if(authorize){
+  const lmao = await registerVisitor(data, loginUser)
+    if (lmao){
+      res.send("Registration request processed, visitor is " + lmao.name)
+    }else{
+      res.send(errorMessage() + "Visitor already exists! Add a visit log instead!")
+    }
+  }else {
+      res.send(errorMessage() + "Not a valid token!")
+    }
+  }
+)
+
+//find visitor GET request
+app.get('/findvisitor', verifyToken, async (req, res)=>{
+  let authorize = req.user//reading t he token for authorisation
+  let data = req.body //requesting the data from body
+  //checking the role of user
+  if (authorize.role){
+    const result = await findVisitor(data,authorize)
+    res.send(result)
+  }else{
+    res.send(errorMessage() + "Not a valid token!") 
+  }
+  })
+
+async function findUser(newdata) {
+  //verify if there is duplicate username in databse
+  const match = await user.find({user_id : newdata.user_id},{projection: {password: 0, _id : 0}}).next()
+  return (match)
+}
+
+async function registerUser(newdata) {
+  //verify if there is duplicate username in databse
+  const match = await user.find({user_id : newdata.user_id}).next()
+    if (match) {
+      return 
+    } else {
+      //encrypt password by hashing
+      const hashed = await encryption(newdata.password)
+      // add info into database
+      await user.insertOne({
+        "user_id": newdata.user_id,
+        "password": hashed,
+        "name": newdata.name,
+        "unit": newdata.unit,
+        "hp_num" : newdata.hp_num,
+        "role" : newdata.role
+      })
+  const newUser=await user.find({user_id : newdata.user_id}).next()
+  return (newUser)
+}}
+    
+async function updateUser(data) {
+
+  result = await user.findOneAndUpdate({user_id : data.user_id},{$set : data}, {new: true})
+  if(result.value == null){
+    return 
+  }else{
+    return (result)
+  }
+}
+
+async function deleteUser(data) {
+  //verify if username is already in databse
+  success = await user.deleteOne({user_id : data.user_id})
+  return (success) // return success message
+}
+
+async function registerVisitor(newdata, currentUser) {
+  //verify if there is duplciate ref_num
+  const match = await visitor.find({"ref_num": newdata.ref}).next()
+    if (match) {
+      return 
+    } else {
+      // add info into database
+      await visitor.insertOne({
+        "ref_num" : newdata.ref,
+        "name": newdata.name,
+        "IC_num": newdata.IC_num,
+        "car_num": newdata.car_num,
+        "hp" : newdata.hp_num,
+        "pass": newdata.pass,
+        "category" : newdata.category,
+        "visit_date" : newdata.date,
+        "unit" : newdata.unit,
+        "user_id" : currentUser
+      })
+          return (newdata)
+    }  
+}
+
+async function findVisitor(newdata, currentUser){
+  if (currentUser.role == "resident"){
+    filter=Object.assign(newdata, {"user_id" : currentUser.user_id})
+    match = await visitor.find(filter, {projection: {_id :0}}).toArray()
+  }else if (currentUser.role == "security" || currentUser.role == "admin"){
+    match = await visitor.find(newdata).toArray()
+  }
+  if (match.length != 0){
+    return (match)
+  } else{
+    return (errorMessage() + "Visitor does not exist!")
+  }
+}
+
+//verify generated tokens
+function verifyToken(req, res, next){
+  let header = req.headers.authorization
+  let token = header.split(' ')[1] //checking header
+  jwt.verify(token,'UltimateSuperMegaTitanicBombasticGreatestBestPOGMadSuperiorTheOneandOnlySensationalSecretPassword',function(err,decoded){
+    if(err) {
+      res.send(errorMessage() + "Token is not valid D:, go to the counter to exchange")
+      //return
+    }
+    req.user = decoded // bar
+
+    next()
+  });
+}
+
+//bcrypt functions to generate a hashed password
+async function encryption(data){
+  const salt= await bcrypt.genSalt(saltRounds)
+  const hashh = await bcrypt.hash(data,salt)
+  return hashh
+}
+
   function errorMessage(){
     const x = Math.floor(Math.random()*6)
     if (x == 0){
--- swagger.js
+++ swagger.js
@@ -1,81 +1,350 @@
 const document={
-    swagger: "2.0",
-    info: {
-      version: "1.0.0",
-      title: "Visitor Management API",
-      description: "API for handling visitor management"
-    },
-    "paths": {
-      "/login": {
-        "get": {
-          "summary": "User login",
-          "description": "Authenticate a user and generate a JWT token",
-          "produces": ["application/json"],
-          "parameters": [
-            {
-              "name": "user_id",
-              "in": "query",
-              "description": "User ID",
-              "required": true,
-              "type": "string"
-            },
-            {
-              "name": "password",
-              "in": "query",
-              "description": "User password",
-              "required": true,
-              "type": "string"
-            }
-          ],
-          "responses": {
-            "200": {
-              "description": "Successful login",
-              "schema": {
-                "type": "object",
-                "properties": {
-                  "message": {
-                    "type": "string",
-                    "example": "User has logged in!"
-                  },
-                  "welcome": {
-                    "type": "string",
-                    "example": "Welcome John Doe!"
-                  },
-                  "token": {
-                    "type": "string",
-                    "example": "your_generated_jwt_token"
-                  }
-                }
-              }
-            },
-            "400": {
-              "description": "Invalid login credentials",
-              "schema": {
-                "type": "object",
-                "properties": {
-                  "error": {
-                    "type": "string",
-                    "example": "No such user ID found D:"
-                  }
-                }
-              }
-            },
-            "401": {
-              "description": "Authentication failed",
-              "schema": {
-                "type": "object",
-                "properties": {
-                  "error": {
-                    "type": "string",
-                    "example": "Wrong password D: Forgotten your password?"
-                  }
+  swagger: "2.0",
+  info: {
+    version: "1.0.0",
+    title: "Visitor Management API",
+    description: "API for handling visitor management"
+  },
+  "paths": {
+    "/login": {
+      "post": {
+        "summary": "Authenticate user",
+        "description": "Authenticate a user based on provided credentials",
+        "tags": ["Authentication"],
+        "parameters": [
+          {
+            "name": "body",
+            "in": "body",
+            "description": "User credentials for authentication",
+            "required": true,
+            "schema": {
+              "type": "object",
+              "properties": {
+                "user_id": {
+                  "type": "string",
+                  "description": "User ID",
+                  "example": "john_doe"
+                },
+                "password": {
+                  "type": "string",
+                  "description": "User password",
+                  "example": "password123"
                 }
               }
             }
           }
+        ],
+    // "/login": {
+    //   "get": {
+    //     "summary": "User login",
+    //     "description": "Authenticate a user and generate a JWT token",
+    //     "tags": ["User Management"],
+    //     "produces": ["application/json"],
+    //     "parameters": [
+    //       {
+    //         "name": "user_id",
+    //         "in": "query",
+    //         "description": "User ID",
+    //         "required": true,
+    //         "type": "string"
+    //       },
+    //       {
+    //         "name": "password",
+    //         "in": "query",
+    //         "description": "User password",
+    //         "required": true,
+    //         "type": "string"
+    //       }
+    //     ],
+    "responses": 
+    {
+      
+      "200": {
+        
+        "description": "Authentication successful",
+        "schema": {
+          "type": "object",
+          "properties": {
+            "success": { "type": "boolean", "example": true },
+            "verify": { "type": "object" },  // The user object
+            "token": { "type": "string", "example": "generated_token" }
+          }
+        }
+      },
+      "401": {
+        "description": "Authentication failed",
+        "schema": {
+          "type": "object",
+          "properties": {
+            "success": { "type": "boolean", "example": false },
+            "error": { "type": "string" }  // Error message
+          }
+        }
+      },
+      "500": {
+        "description": "Internal server error",
+        "schema": {
+          "type": "object",
+          "properties": {
+            "success": { "type": "boolean", "example": false },
+            "error": { "type": "string" }  // Error message
+          }
         }
       }
     }
+        // "responses": {
+        //   "200": {
+        //     "description": "Successful login",
+        //     "schema": {
+        //       "type": "object",
+        //       "properties": {
+        //         "message": {
+        //           "type": "string",
+        //           "example": "User has logged in!"
+        //         },
+        //         "welcome": {
+        //           "type": "string",
+        //           "example": "Welcome John Doe!"
+        //         },
+        //         "token": {
+        //           "type": "string",
+        //           "example": "your_generated_jwt_token"
+        //         }
+        //       }
+        //     }
+        //   },
+        //   "400": {
+        //     "description": "Invalid login credentials",
+        //     "schema": {
+        //       "type": "object",
+        //       "properties": {
+        //         "error": {
+        //           "type": "string",
+        //           "example": "No such user ID found D:"
+        //         }
+        //       }
+        //     }
+        //   },
+        //   "401": {
+        //     "description": "Authentication failed",
+        //     "schema": {
+        //       "type": "object",
+        //       "properties": {
+        //         "error": {
+        //           "type": "string",
+        //           "example": "Wrong password D: Forgotten your password?"
+        //         }
+        //       }
+        //     }
+        //   }
+        
+      }
+    },
+  
+  "/finduser": {
+    "get": {
+      "summary": "Find user by user_id",
+      "description": "Find a user by user_id (admin only)",
+      "tags": ["User Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "user_id",
+          "in": "query",
+          "description": "User ID to find",
+          "required": true,
+          "type": "string"
+        }
+      ],
+      "responses": {
+        "200": {"description": "Successful response"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "404": {"description": "User not found"}
+      }
+    }
+  },
+  "/registeruser": {
+    "post": {
+      "summary": "Register a new user",
+      "description": "Register a new user (admin only)",
+      "tags": ["User Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "User",
+          "in": "body",
+          "description": "User details for registration",
+          "required": true,
+          "schema": {"$ref": "#/definitions/User"}
+        }
+      ],
+      "responses": {
+        "201": {"description": "User registered successfully"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "409": {"description": "User already exists"}
+      }
+    }
+  },
+  "/updateuser": {
+    "patch": {
+      "summary": "Update user information",
+      "description": "Update user information (admin only)",
+      "tags": ["User Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "User",
+          "in": "body",
+          "description": "User details for update",
+          "required": true,
+          "schema": {"$ref": "#/definitions/User"}
+        }
+      ],
+      "responses": {
+        "200": {"description": "User updated successfully"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "404": {"description": "User not found"}
+      }
+    }
+  },
+  "/deleteuser": {
+    "delete": {
+      "summary": "Delete user by user_id",
+      "description": "Delete a user by user_id (admin only)",
+      "tags": ["User Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "user_id",
+          "in": "query",
+          "description": "User ID to delete",
+          "required": true,
+          "type": "string"
+        }
+      ],
+      "responses": {
+        "200": {"description": "User deleted successfully"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "404": {"description": "User not found"}
+      }
+    }
+  },
+  "/registervisitor": {
+    "post": {
+      "summary": "Register a new visitor",
+      "description": "Register a new visitor (admin only)",
+      "tags": ["Visitor Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "Visitor",
+          "in": "body",
+          "description": "Visitor details for registration",
+          "required": true,
+          "schema": {"$ref": "#/definitions/Visitor"}
+        }
+      ],
+      "responses": {
+        "201": {"description": "Visitor registered successfully"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "409": {"description": "Visitor already exists"}
+      }
+    }
+  },
+  "/findvisitor": {
+    "get": {
+      "summary": "Find visitors based on criteria",
+      "description": "Find visitors based on specified criteria (admin or security only)",
+      "tags": ["Visitor Management"],
+      "parameters": [
+        {
+          "name": "Authorization",
+          "in": "header",
+          "description": "JWT Authorization token",
+          "required": true,
+          "type": "string"
+        },
+        {
+          "name": "criteria",
+          "in": "query",
+          "description": "Criteria to search for visitors",
+          "required": true,
+          "type": "string"
+        }
+      ],
+      "responses": {
+        "200": {"description": "Successful response"},
+        "401": {"description": "Unauthorized"},
+        "403": {"description": "Forbidden"},
+        "404": {"description": "No visitors found"}
+      }
+    }
   }
+},
+"definitions": {
+  "User": {
+    "type": "object",
+    "properties": {
+      "user_id": {"type": "string"},
+      "password": {"type": "string"},
+      "name": {"type": "string"},
+      "unit": {"type": "string"},
+      "hp_num": {"type": "string"},
+      "role": {"type": "string"}
+    }
+  },
+  "Visitor": {
+    "type": "object",
+    "properties": {
+      "ref_num": {"type": "string"},
+      "name": {"type": "string"},
+      "IC_num": {"type": "string"},
+      "car_num": {"type": "string"},
+      "hp_num": {"type": "string"},
+      "pass": {"type": "string"},
+      "category": {"type": "string"},
+      "visit_date": {"type": "string"},
+      "unit": {"type": "string"},
+      "user_id": {"type": "string"}
+    }
+  }
+}
+}
 
-  module.exports = document;
+
+module.exports = document;
